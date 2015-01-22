9f29660d1aa96a58e3632d48ad36dbba5bbae896
Jakob Larsson <symphonymdev@gmail.com>
2015-01-22 11:27:40 +0100

    Set up SFML and create basic test program

--- Brainless/Brainless/main.cpp
+++ Brainless/Brainless/main.cpp
@@ -1,8 +1,28 @@
-
+#include <SFML\Graphics.hpp>
 
 int main()
 {
-	//comment
-	// comment something else
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Brainless");
+	sf::CircleShape circle(100.f);
+	circle.setFillColor(sf::Color::Green);
+
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+
+		}
+
+		window.clear(sf::Color::Black);
+
+		window.draw(circle);
+		window.display();
+	}
+	
+
 	return 0;
 }
