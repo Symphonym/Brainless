3ed536444d5427d5a37b8fc4f4584b9ab4eae687
SvanteAlmbring <svante.almbring@gmail.com>
2015-01-26 10:15:43 +0100

    failed update fix

--- Brainless/Brainless/FileSave.cpp
+++ Brainless/Brainless/FileSave.cpp
@@ -1,56 +1,64 @@
-#include <ostream>
 #include <fstream>
-#include "FileSave.h"
+#include <string>
+
+//
 #include "Constants.h"
+#include "FileSave.h"
 #include "TileMap.h"
 #include "Tile.h"
 
-void FileSave::saveMap(TileMap& map)
+void FileSave::saveMap(TileMap* map, int stage_number)
 {
-	int map_width = Constants::MapWidth, map_height = Constants::MapHeight;
 	//File variables
-	std::ofstream file_write;
-	file_write.open("Map1.fmap");
-	const int file_size = 2 + map_width*map_height;
+	std::ofstream file_write; file_write.open(std::to_string(stage_number) +".fmap");
+	const int file_size = 2 + Constants::MapWidth*Constants::MapHeight;
 	unsigned char * file_content = new unsigned char[file_size];
 	int file_at = 0;
+
 	// - Save all tiles
-	file_content[0] = map_width;
-	file_content[1] = map_height;
+	file_content[0] = Constants::MapWidth;
+	file_content[1] = Constants::MapHeight;
 	file_at = 2;
-	for (int x = 0; x < map_width; x++)
+	for (int x = 0; x < Constants::MapWidth; x++)
 	{
-		for (int y = 0; y < map_height; y++)
+		for (int y = 0; y < Constants::MapHeight; y++)
 		{
-			file_content[file_at] = static_cast<int>(map.getTile(x, y).getType());
+			file_content[file_at] = static_cast<int>(map->getTile(x, y).getType());
 			file_at++;
 		}
 	}
+	//write to file
 	file_write.write((const char*)file_content, file_at);
 	// - Save all items
-	//2xbyte x
-	//2xbyte y
+	// byte - x
+	// byte - y
+	// byte - sprite
+	// byte - code
 	// - Save
 }
 
-void FileSave::loadMap()
+void FileSave::loadMap(TileMap* map, int stage_number)
 {
-	std::ifstream file_read("test.txt", std::ifstream::binary);
-	file_read.open("Map1.fmap");
-
-	//Find size then go back to start
-	file_read.seekg(0, file_read.end);
+	//File variables
+	std::ifstream file_read; file_read.open(std::to_string(stage_number) + ".fmap");
+	file_read.seekg(0, file_read.end); //Find size of the file then go back to start
 	const int file_size = file_read.tellg();
 	file_read.seekg(0, file_read.beg);
-
 	unsigned char * file_content = new unsigned char[file_size];
+	int file_at = 0;
+
+	//Read from file
 	file_read.read((char*)file_content, file_size);
 
 	int map_width = file_content[0];
 	int map_ehight = file_content[1];
-
-	for (int i = 0; i < file_size; i++)
+	file_at = 2;
+	for (int x = 0; x < Constants::MapWidth; x++)
 	{
-		//std::cout << i << ":" << (int)file_content[i] << std::endl;
+		for (int y = 0; y < Constants::MapHeight; y++)
+		{
+			map->getTile(x, y).setType(static_cast<Tile::TileTypes>(file_content[file_at]));
+			file_at++;
+		}
 	}
 }
