4e7ac7ce3ef5e4b74c855b6dd94e13cf4108a3ca
SvanteAlmbring <svante.almbring@gmail.com>
2015-01-27 12:57:39 +0100

    i/o change

--- Brainless/Brainless/FileSave.cpp
+++ Brainless/Brainless/FileSave.cpp
@@ -19,7 +19,7 @@ void FileSave::saveMap(Level* stage, int stage_number)
 	TileMap& map = stage->getTileMap();
 	//File variables
 	std::ofstream file_write; file_write.open(std::to_string(stage_number) + ".fmap");
-	const int file_size = (2 + Constants::MapWidth*Constants::MapHeight) + 1;//+ 6 * item_array.size();
+	const int file_size = (2 + Constants::MapWidth*Constants::MapHeight) + 1;//+ 8 * item_array.size();
 	unsigned char * file_content = new unsigned char[file_size];
 	int file_at = 0;
 
@@ -37,31 +37,35 @@ void FileSave::saveMap(Level* stage, int stage_number)
 	}
 	//Save all items to array
 	std::vector<Item*> item_array;
-	file_content[file_at] = item_array.size();
+	file_content[file_at] = item_array.size(); file_at++;
 	for (int i = 0; i < item_array.size(); i++)
 	{
-		file_content[file_at + 0] = floor(item_array[i]->getPosition().x / 255);
-		file_content[file_at + 1] = (int)(item_array[i]->getPosition().x) % 255;
-		file_content[file_at + 2] = floor(item_array[i]->getPosition().y / 255);
-		file_content[file_at + 3] = (int)(item_array[i]->getPosition().y) % 255;
-		file_content[file_at + 4] = item_array[i]->getID();
-		file_content[file_at + 5] = item_array[i]->getSyncID();
-		file_at += 6;
+		if (item_array[i]->getPosition().x < 0)
+			file_content[file_at + 0] = 0; else file_content[file_at + 0] = -1;
+		file_content[file_at + 1] = floor(item_array[i]->getPosition().x / 255);
+		file_content[file_at + 2] = (int)(item_array[i]->getPosition().x) % 255;
+		if (item_array[i]->getPosition().y < 0)
+			file_content[file_at + 3] = 0; else file_content[file_at + 3] = -1;
+		file_content[file_at + 4] = floor(item_array[i]->getPosition().y / 255);
+		file_content[file_at + 5] = (int)(item_array[i]->getPosition().y) % 255;
+		file_content[file_at + 6] = item_array[i]->getID();
+		file_content[file_at + 7] = item_array[i]->getSyncID();
+		file_at += 8;
 	}
 	//Save all textures to array
+	file_content[file_at] file_at++;
 	for (int i = 0; i < stage->getDecorations().size(); i++)
 	{
-		int string_lenght = 0;
+		int string_lenght = stage->getDecorations();
 		file_content[file_at] = stage->getDecorations()[i].first.getPosition().x;
 		file_content[file_at + 1] = stage->getDecorations()[i].first.getPosition().y;
 		file_content[file_at + 2] = string_lenght;
 		file_at++;
-		for (int j = 0; j < string_lenght;j++)
+		for (int j = 0; j < string_lenght; j++)
 		{
 			file_content[i] = 'b';
 			file_at++;
 		}
-		file_content[i] = stage->getDecorations[string_lenght];
 		file_at += string_lenght;
 	}
 	//write array to file
