4c4972bc6a5b7c39e857b77d0923a7c665999805
Jakob Larsson <symphonymdev@gmail.com>
2015-01-29 11:42:50 +0100

    Moved code from game to level and added combine/crafting data for items. Added index text for grid mode in editor as well.

--- Brainless/Brainless/Level.h
+++ Brainless/Brainless/Level.h
@@ -4,7 +4,7 @@
 #include "TileMap.h"
 #include "Item.h"
 #include "LevelSprite.h"
-#include <SFML\Graphics.hpp>
+#include "Unit.h"
 #include <memory>
 #include <vector>
 
@@ -14,8 +14,13 @@ public:
 
 	typedef std::unique_ptr<TileMap> MapPtr;
 	typedef std::unique_ptr<Item> ItemPtr;
+	typedef std::unique_ptr<Unit> UnitPtr;
 
 	Level();
+
+	// Adds a unit to the level and returns a pointer to it
+	Unit* addUnit(UnitPtr unit);
+
 
 	void update(float deltaTime);
 	void draw(const sf::View &cameraView);
@@ -23,12 +28,16 @@ public:
 	TileMap& getTileMap();
 	std::vector<ItemPtr>& getItems();
 	std::vector<LevelSprite>& getDecorations();
+	std::vector<UnitPtr>& getUnits();
 
 private:
+
+	void updateUnitCollision(float deltaTime);
 
 	MapPtr m_tileMap; // Tilemap of all the tiles in the level
 	std::vector<ItemPtr> m_items; // Items in the level
 	std::vector<LevelSprite> m_sprites; // Decoration sprites in the level
+	std::vector<UnitPtr> m_units; // Units in the level
 
 
 	// List of units
