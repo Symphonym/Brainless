816d3529eb2e348e23b9b2104d3a7f034cf64365
SvanteAlmbring <svante.almbring@gmail.com>
2015-02-04 13:48:41 +0100

    Fixed walking Zombie walking physics

--- Brainless/Brainless/WalkingZombie.cpp
+++ Brainless/Brainless/WalkingZombie.cpp
@@ -25,7 +25,66 @@ m_animState(noAnimation)
 
 void WalkingZombie::updateTask(float deltaTime)
 {
-	m_currentDisparity = m_position.x - m_minPositionX;
+	float speedTurnAround = 10;
+	float speedStartAcc = 300;
+	float speedNormalAcc = 100;
+	float speedSlowDown = 6;
+
+	float startAccBreakpoint = 150;
+	float minSpeedBeforeStop = 10;
+
+	bool slowDown = true;
+	//Walking Zombie's AI
+	if (m_position.x < m_minPositionX)
+		m_direction = right;
+	else if (m_maxPositionX < m_minPositionX + (m_position.x - m_minPositionX))
+		m_direction = left;
+	//Note for simplicity: This is practically physics code and could be used for all unit classes instead of copied.
+	//Left
+	if (m_direction==left)
+	{
+		if (20 < m_speed.x) //wrong direcion - slow character
+		{
+			m_acceleration.x = -m_speed.x * speedTurnAround;
+		}
+		else
+		{
+			if (abs(m_speed.x) < startAccBreakpoint)
+				m_acceleration.x = -speedStartAcc;
+			else
+				m_acceleration.x = -speedNormalAcc;
+		}
+		slowDown = false;
+	}
+	//Right
+	if (m_direction == right)
+	{
+		if (m_speed.x < -20) //wrong direcion - slow character
+		{
+			m_acceleration.x = -m_speed.x * speedTurnAround;
+		}
+		else
+		{
+			if (abs(m_speed.x) < startAccBreakpoint)
+				m_acceleration.x = speedStartAcc;
+			else
+				m_acceleration.x = speedNormalAcc;
+		}
+		slowDown = false;
+	}
+	if (slowDown)
+	{
+		//small values = stop totally
+		if (m_speed.x < minSpeedBeforeStop && m_speed.x > -minSpeedBeforeStop)
+		{
+			m_speed.x = 0;
+			m_acceleration.x = 0;
+		}
+		//slow
+		else m_acceleration.x = -m_speed.x * speedSlowDown;
+	}
+	
+	/*m_currentDisparity = m_position.x - m_minPositionX;
 	//Left
 	if (m_direction == left)
 	{
@@ -49,8 +108,7 @@ void WalkingZombie::updateTask(float deltaTime)
 		}
 		else
 		m_speed.x = 100;
-	}
-
+	}*/
 }
 
 void WalkingZombie::updateAnimation(float deltaTime)
