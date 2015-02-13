ba71bddefca5b13d1c496e4bc96af94dc18ac0ee
Jakob Larsson <symphonymdev@gmail.com>
2015-02-13 11:35:27 +0100

    Started work on conducting item base class

---
+++ Brainless/Brainless/ConductingItem.h
@@ -0,0 +1,16 @@
+#ifndef INCLUDED_CONDUCTING_ITEM_H
+#define INCLUDED_CONDUCTING_ITEM_H
+
+#include "Item.h"
+
+class ConductingItem : public Item
+{
+public:
+
+	ConductingItem(const std::string &itemName, const std::string &textureName, int id);
+
+
+
+};
+
+#endif
