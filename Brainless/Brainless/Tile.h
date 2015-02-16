544d51fc261b1a917443c1887d0288116405df8e
Twilice <molkiar@gmail.com>
2015-02-16 14:47:17 +0100

    alla tiles tillagda, vissa bilder saknas fort. kommer nästa commit

--- Brainless/Brainless/Tile.h
+++ Brainless/Brainless/Tile.h
@@ -18,25 +18,50 @@ public:
 		//Short är tales som angränsar en annan tile "snett"
 		//Alone är tiles som inte angränsar till något verticalt/horiontalt
 		Road_Top_Right = 6,
+		Road_Top_Right_Corner = 56,
+
 		Road_Top_Left = 7,
 		Road_Top_Left_Short = 8,
-		Road_Tilt = 9,
+		Road_Top_Left_Corner = 55,
+
 		Road_Top = 10,
 		Road_Top_Alone = 11,
 		Road_Top_Short = 12,
+		Road_Top_Corner_Left = 52,
+		Road_Top_Corner_Right = 53,
+		Road_Top_Corner_Full = 54,
+
 		Road_Right = 13,
 		Road_Right_Alone = 14,
+		Road_Right_Corner_Down = 49,
+		Road_Right_Corner_Top = 50,
+		Road_Right_Corner_Full = 51,
+
 		Road_Middle = 15,
 		Road_Middle_Alone = 16,
 		Road_Middle_Alone_Horizontal = 17,
 		Road_Middle_Alone_Vertical = 18,
+
 		Road_Left = 19,
 		Road_Left_Short = 20,
 		Road_Left_Alone = 21,
+		Road_Left_Corner_Down = 46,
+		Road_Left_Corner_Top = 47,
+		Road_left_Corner_Full = 48,
+
 		Road_Down_Right = 22,
-		Road_Down_Left = 23, 
+		Road_Down_Right_Corner = 45,
+		Road_Down_Left = 23,
+		Road_Down_Left_Corner = 44,
+
 		Road_Down = 24,
 		Road_Down_Alone = 28,
+		Road_Down_Corner_Left = 41,
+		Road_Down_Corner_Right = 42,
+		Road_Down_Corner_Full = 43,
+
+		Road_Tilt = 9,
+
 		Road_Corner_TopLeft = 25,
 		Road_Corner_TopRight = 26,
 		Road_Corner_TopLeft_TopRight = 27,
