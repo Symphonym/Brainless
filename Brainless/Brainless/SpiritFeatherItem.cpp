a12e9c1f01cac52036ae129560210dd477d0081a
kevin <kevin.krona@gmail.com>
2015-02-17 15:40:12 +0100

    added animation to magic feather

--- Brainless/Brainless/SpiritFeatherItem.cpp
+++ Brainless/Brainless/SpiritFeatherItem.cpp
@@ -1,14 +1,20 @@
 #include "SpiritFeatherItem.h"
 #include "Unit.h"
 #include "Game.h"
+#include "Renderer.h"
+
 
 SpiritFeatherItem::SpiritFeatherItem(int id)
 :
-Item("Spirit Feather", "KEVIN_INSERT_SPRITESHIT_HERE", id)
+Item("Spirit Feather", "SpiritFeather", id),
+m_animation(100,100)
 {
-
+	m_collisionSize = sf::Vector2f(40, 100);
+	m_collisionOffset = sf::Vector2f(30, 0);
 	m_examineString = "It's a feather that's mystically hovering around";
 	m_useString = "I don't think I should do that";
+	m_animation.loop(0, 7, 0, 10);
+	getSprite().setTextureRect(m_animation.getRectangle(0));
 }
 
 bool SpiritFeatherItem::onCollisionWithUnit(Unit &unit, Game &game)
@@ -22,9 +28,16 @@ bool SpiritFeatherItem::onCollisionWithUnit(Unit &unit, Game &game)
 	else
 		return false;
 }
+
+void SpiritFeatherItem::update(float deltaTime, Game &game)
+{
+	getSprite().setTextureRect(m_animation.getRectangle(deltaTime));
+}
+
 void SpiritFeatherItem::draw()
 {
 	// draw animation with getSprite()
+	Renderer::instance().drawAbove(getSprite());
 }
 
 Item* SpiritFeatherItem::clone()
