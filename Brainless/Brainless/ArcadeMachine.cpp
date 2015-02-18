2539a47713f36d59c02ebbef0f8e2f41df6aa78b
kevin <kevin.krona@gmail.com>
2015-02-18 15:25:17 +0100

    Worked on beach party

--- Brainless/Brainless/ArcadeMachine.cpp
+++ Brainless/Brainless/ArcadeMachine.cpp
@@ -4,6 +4,7 @@
 #include "Utility.h"
 #include "ArcadeGame.h"
 #include "ResourceLoader.h"
+#include "BeachParty.h"
 
 ArcadeMachine::ArcadeMachine(StateMachine &machine)
 :
@@ -20,7 +21,7 @@ m_playingGame(false)
 	//	0);
 
 	// ADD YOUR GAMES HERE
-	//m_games[0] = GamePtr(new ...);
+	m_games[0] = GamePtr(new BeachParty(*this));
 	//m_games[1] = GamePtr(new ...);
 	//m_games[2] = GamePtr(new ...);
 	//m_games[3] = GamePtr(new ...);
