94689599689e0a256a62ab4f36a31ffd46fc048e
kevin <kevin.krona@gmail.com>
2015-02-18 14:17:50 +0100

    started on an arcade game

---
+++ Brainless/Brainless/BeachParty.cpp
@@ -0,0 +1,19 @@
+#include "BeachParty.h"
+#include "ResourceLoader.h"
+
+BeachParty::BeachParty(ArcadeMachine &machine)
+:
+ArcadeGame(machine, "Åke's Beach Party"),
+m_turtleAnimation(50,50),
+m_crabAnimation(40,40),
+m_frieAnimation(40,40)
+{
+	m_turtleSprite.setTexture(ResourceLoader::instance().retrieveTexture("turtlesheet"));
+	m_crabSprite.setTexture(ResourceLoader::instance().retrieveTexture("crabsheet"));
+	m_frieSprite.setTexture(ResourceLoader::instance().retrieveTexture("friesheet"));
+}
+
+void BeachParty::update(float deltaTime)
+{
+
+}
