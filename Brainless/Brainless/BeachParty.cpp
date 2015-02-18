109bc6ba4bf7617ac035c990c77ed00b4276a116
kevin <kevin.krona@gmail.com>
2015-02-18 14:19:15 +0100

    fixed thing

--- Brainless/Brainless/BeachParty.cpp
+++ Brainless/Brainless/BeachParty.cpp
@@ -8,9 +8,9 @@ m_turtleAnimation(50,50),
 m_crabAnimation(40,40),
 m_frieAnimation(40,40)
 {
-	m_turtleSprite.setTexture(ResourceLoader::instance().retrieveTexture("turtlesheet"));
+	/*m_turtleSprite.setTexture(ResourceLoader::instance().retrieveTexture("turtlesheet"));
 	m_crabSprite.setTexture(ResourceLoader::instance().retrieveTexture("crabsheet"));
-	m_frieSprite.setTexture(ResourceLoader::instance().retrieveTexture("friesheet"));
+	m_frieSprite.setTexture(ResourceLoader::instance().retrieveTexture("friesheet"));*/
 }
 
 void BeachParty::update(float deltaTime)
