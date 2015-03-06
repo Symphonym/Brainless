5457c1beba64b54234174c803e9a7a53043c64ef
Jakob Larsson <symphonymdev@gmail.com>
2015-03-06 13:00:38 +0100

    Added new new game intro screen

--- Brainless/Brainless/NewGameIntro.cpp
+++ Brainless/Brainless/NewGameIntro.cpp
@@ -10,18 +10,29 @@ NewGameIntro::NewGameIntro(StateMachine &machine)
 State(machine),
 m_currentIndex(0),
 m_curFade(0),
-m_maxFade(1)
+m_maxFade(6)
 {
 	ResourceLoader::instance().loadResourceFile("loadfiles/ResourceLoad_NewGameIntro.txt");
 	//Notification::instance().write("The game has zombies and shit, pretty crazy");
 
-	pushIntroText("LORE");
-	pushIntroText("MORE LORE");
+	pushIntroText("The change was slow...");
+	pushIntroText("Peoples minds just shut off and no one knew what to do...");
+	pushIntroText("Society slowed down, stuttered, and stopped...");
+	pushIntroText("No one was safe, and no one cared...");
+	pushIntroText("A few had secluded themselves in hope of avoiding the devastating change...");
 }
 
 void NewGameIntro::events(const sf::Event &event)
 {
-
+	if (event.type == sf::Event::KeyReleased)
+	{
+		// Allow skipping of the intro with escape
+		if (event.key.code == sf::Keyboard::Escape)
+		{
+			m_machine.popState();
+			m_machine.pushState<Game>();
+		}
+	}
 }
 void NewGameIntro::update(float deltaTime)
 {
@@ -68,11 +79,11 @@ void NewGameIntro::update(float deltaTime)
 	
 
 
-	if (!Notification::instance().isShown())
-	{
+	//if (!Notification::instance().isShown())
+	//{
 		//m_machine.popState();
 		//m_machine.pushState<Game>();
-	}
+	//}
 }
 void NewGameIntro::draw()
 {
@@ -96,7 +107,7 @@ void NewGameIntro::pushIntroText(const std::string &text)
 	sf::Text introText;
 	introText.setFont(ResourceLoader::instance().retrieveFont("DefaultFont"));
 	introText.setString(text);
-	introText.setCharacterSize(150);
+	introText.setCharacterSize(30);
 	introText.setColor(sf::Color(255, 255, 255, 0));
 	introText.setPosition(
 		m_window.getSize().x / 2.f - introText.getGlobalBounds().width / 2.f,
