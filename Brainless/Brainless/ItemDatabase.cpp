88c15c060a4ab87dd95d005bfb7987890e8f5b4e
Twilice <molkiar@gmail.com>
2015-03-11 14:54:56 +0100

    nya levers

--- Brainless/Brainless/ItemDatabase.cpp
+++ Brainless/Brainless/ItemDatabase.cpp
@@ -46,6 +46,8 @@
 #include "CrateItem.h"
 #include "Lamp.h"
 #include "StartDialougeItem.h"
+#include "LeverItem.h"
+#include "LeverSideItem.h"
 
 ItemDatabase::ItemDatabase()
 {
@@ -122,6 +124,8 @@ ItemDatabase::ItemDatabase()
 	//addItem(ItemPtr(new GhostItem("dialogues/LEVEL2.txt", 48)));
 	addItem(ItemPtr(new Lamp(49)));
 	addItem(ItemPtr(new StartDialougeItem(50)));
+	addItem(ItemPtr(new LeverItem(true, 51)));
+	addItem(ItemPtr(new LeverSideItem(true, 52)));
 }
 
 void ItemDatabase::addItem(ItemPtr itemPtr)
