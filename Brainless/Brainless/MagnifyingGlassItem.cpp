4876d921a45a824b0611173ce57b5cf10e86ea48
Twilice <molkiar@gmail.com>
2015-03-25 09:34:43 +0100

    annan string

--- Brainless/Brainless/MagnifyingGlassItem.cpp
+++ Brainless/Brainless/MagnifyingGlassItem.cpp
@@ -9,8 +9,8 @@ Item("Magnifying Glass","MagnifyingGlassInv", "MagnifyingGlass", id)
 {
 	m_lootable = true;
 	m_usable = false;
-	m_useString = "Don't think I can use that, just like that.";
-	m_examineString = "Bottle music is, or rather was, my passion. Let's start a band!";
+	m_useString = "I should pick it up before using.";
+	m_examineString = "Childhood memories; burning ants, smelling their burnt carcasses, it's all coming back to me.";
 
 	m_magnifyingView = sf::View(sf::FloatRect(0, 0, 0.25f, 0.25f));
 	m_background.setTexture(ResourceLoader::instance().retrieveTexture("MagnifyingGlassBG"));
